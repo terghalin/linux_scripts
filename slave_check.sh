3cca45fa351847439fe7a574fd5d4fed7a67924f
terghalin <terghalin@gmail.com>
2015-08-21 20:37:20 +0300

    Update slave_check.sh
    DRY! Deleted old-style binlog replication.

--- slave_check.sh
+++ slave_check.sh
@@ -1,4 +1,4 @@
-SCHECK=$(mysql -h 172.16.0.50 -u root --password=********** -e "SHOW SLAVE STATUS\G;" | grep Slave_SQL_Running)
+SCHECK=$(mysql -h 172.16.0.50 -u root --password=XXXXXXXX -e "SHOW SLAVE STATUS\G;" | grep Slave_SQL_Running)
 echo $SCHECK
 
 #SCHECK="No"  # just for testing bad thingies
@@ -10,11 +10,10 @@ elif [[ $SCHECK == *"No"* ]]
  then
   echo "Slave SQL is not running!"
   ### Убираем из сообщений об ошибке непечатные символы для удобного чтения
-  LASTERR=$(mysql -h 172.16.0.50 -u root --password=********** -e "SHOW SLAVE STATUS\G;" | grep Last_SQL_Error: | tr -dc '[:print:]')
+  LASTERR=$(mysql -h 172.16.0.50 -u root --password=XXXXXXXX -e "SHOW SLAVE STATUS\G;" | grep Last_SQL_Error: | tr -dc '[:print:]')
   echo $LASTERR
   echo
-  # read -p "Should we start rebuilding slave? (y/n) " -n 1 -r  # accept one character for input (without Enter)
-  read -p "Do you want to REBUILD slave? This operation will lock tables on master. (yes/no) " REPLY
+  read -p "Do you want to RESET slave? (yes/no) " REPLY
   case "$REPLY" in
     yes );;
     n|N|no ) echo "Aborted";;
@@ -23,44 +22,30 @@ elif [[ $SCHECK == *"No"* ]]
   echo
   if [[ $REPLY == *"yes"* ]]
     then
-     echo "Rebuilding slave..."
-     echo "Resetting master and flushing tables with read lock."
-     mysql -u root -e "
-        RESET MASTER;
-        FLUSH TABLES WITH READ LOCK;"
+     echo "Resetting slave..."
      MASTERPOS=$(mysql -u root -e "SHOW MASTER STATUS;" | awk 'FNR == 2 {print $2}')
      echo "Master binlog position is $MASTERPOS"
      GTID_POS=$(mysql -u root -e "SELECT BINLOG_GTID_POS('mysql-bin.000001', $MASTERPOS);" | awk 'FNR == 2')
      echo "BINLOG_GTID_POS = $GTID_POS"
-     echo "Saving mysql dump to /tmp/ts_drupal.sql ..."
-     mysqldump -u root --lock-all-tables ts_drupal > /tmp/ts_drupal.sql
-     echo "Unlocking master tables..."
-     mysql -u root -e "UNLOCK TABLES";
-     echo "Copying master dump to slave directory /tmp/ts_drupal.sql"
-     scp /tmp/ts_drupal.sql 172.16.0.50:/tmp/ts_drupal.sql
-
      echo "Stopping slave..."
-     mysql -h 172.16.0.50 -u root --password=********** -e "STOP SLAVE;"
-     echo "Loading master mysql dump to slave..."
-     mysql -h 172.16.0.50 -u root --password=********** ts_drupal < /tmp/ts_drupal.sql
-     echo "Syncing slave with master binlog..."
-     mysql -h 172.16.0.50 -u root --password=********** -e "
+     mysql -h 172.16.0.50 -u root --password=XXXXXXXX -e "STOP SLAVE;"
+     echo "Syncing slave with master via GTID..."
+     mysql -h 172.16.0.50 -u root --password=XXXXXXXX -e "
         RESET SLAVE;
         SET GLOBAL gtid_slave_pos = '$GTID_POS';
         CHANGE MASTER TO master_use_gtid=slave_pos,
                 master_host='172.16.0.5',
                 master_user='slave_usr',
-                master_password='**********',
-                master_log_file='mysql-bin.000001',
-                master_log_pos=$MASTERPOS;
+                master_password='XXXXXXXX';
                 START SLAVE;"
      echo "Check if slave SQL is running..."
-     sleep 3
-     SCHECK=$(mysql -h 172.16.0.50 -u root --password=********** -e "SHOW SLAVE STATUS\G;" | grep Slave_SQL_Running)
+     sleep 2
+     SCHECK=$(mysql -h 172.16.0.50 -u root --password=XXXXXXXX -e "SHOW SLAVE STATUS\G;" | grep Slave_SQL_Running)
      echo $SCHECK
 
   else exit
   fi
+  
 else
  echo "Something happend with Slave... :("
  exit
